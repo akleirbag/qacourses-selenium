dbee6a60032013c0c1031a59b4d124d042287bf6
akleirbag <akleirbag@gmail.com>
2017-02-14 22:54:45 +0100

    First test

---
+++ qaSelenium/src/test/java/ru/stqa/training/selenium/FirstTest.java
@@ -0,0 +1,20 @@
+package ru.stqa.training.selenium;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class FirstTest extends TestBase {
+
+    @Test
+    public void test1() {
+        driver.navigate().to("http://www.google.com");
+        driver.findElement(By.name("q")).sendKeys("webdriver");
+        driver.findElement(By.name("btnG")).click();
+        wait.until(titleIs("webdriver - Szukaj w Google"));
+        System.out.println("Test");
+    }
+
+  
+}
