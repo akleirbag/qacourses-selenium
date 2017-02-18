7a42382aa923c47c2d6efd825a759b0e5bd7f02f
akleirbag <akleirbag@gmail.com>
2017-02-18 17:12:55 +0100

    Zadanie 03, skrypt logowania

---
+++ qaSelenium/src/ru/stqa/training/selenium/LoginUserTest.java
@@ -0,0 +1,16 @@
+package ru.stqa.training.selenium;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+public class LoginUserTest extends TestBase{
+
+	
+	@Test
+	public void loginUser(){
+		driver.navigate().to("http://localhost:8080/litecart/admin/");
+		driver.findElement(By.name("username")).sendKeys("admin");
+		driver.findElement(By.name("password")).sendKeys("admin");
+		driver.findElement(By.name(("login"))).click();	
+	}
+}
--- qaSelenium/src/ru/stqa/training/selenium/TestBase.java
+++ qaSelenium/src/ru/stqa/training/selenium/TestBase.java
@@ -10,32 +10,32 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestBase {
 
-    public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
-    public WebDriver driver;
-    public WebDriverWait wait;
+	public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+	public WebDriver driver;
+	public WebDriverWait wait;
 
-    @Before
-    public void start() {
-        if (tlDriver.get() != null) {
-            driver = tlDriver.get();
-            wait = new WebDriverWait(driver, 10);
-            return;
-        }
-        System.setProperty("webdriver.gecko.driver", "C:\\dev\\drivers\\geckodriver.exe");
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability(FirefoxDriver.MARIONETTE, true);
-        driver = new FirefoxDriver(caps);
-        tlDriver.set(driver);
-        System.out.println(((HasCapabilities) driver).getCapabilities());
-        wait = new WebDriverWait(driver, 10);
+	@Before
+	public void start() {
+		if (tlDriver.get() != null) {
+			driver = tlDriver.get();
+			wait = new WebDriverWait(driver, 10);
+			return;
+		}
+		System.setProperty("webdriver.gecko.driver", "C:\\dev\\drivers\\geckodriver.exe");
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setCapability(FirefoxDriver.MARIONETTE, true);
+		driver = new FirefoxDriver(caps);
+		tlDriver.set(driver);
+		System.out.println(((HasCapabilities) driver).getCapabilities());
+		wait = new WebDriverWait(driver, 10);
 
-        Runtime.getRuntime().addShutdownHook(
-                new Thread(() -> { driver.quit(); driver = null; }));
-    }
+		  Runtime.getRuntime().addShutdownHook(
+		        new Thread(() -> { driver.quit(); driver = null; }));
+	}
 
-    @After
-    public void stop() {
-        //driver.quit();
-        //driver = null;
-    }
+	@After
+	public void stop() {
+		driver.quit();
+		driver = null;
+	}
 }
